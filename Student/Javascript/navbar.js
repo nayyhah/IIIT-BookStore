e9d987024e9c9d387737393fa2f604efccef9c9c
Neha Jha <70996315+njha07@users.noreply.github.com>
2021-03-04 21:19:09 +0530

    navbar js

---
+++ Student/Javascript/navbar.js
@@ -0,0 +1,1 @@
+$(".navbar").css({top:"0",width:"100%"}),$(".navbar").affix({offset:{top:function(){return this.top=$(".navbar").offset().top}}}),$(".navbar").on("affix.bs.affix",function(){$(".navbar + *").css("margin-top",$(".navbar").outerHeight(!0))}),$(".navbar").on("affix-top.bs.affix",function(){$(".navbar + *").css({"margin-top":"0"})});
