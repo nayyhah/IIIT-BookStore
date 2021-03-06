1614b9aaedd3bcd2e49d15ff975223015a29ef70
Neha Jha <70996315+njha07@users.noreply.github.com>
2021-03-06 12:25:39 +0530

    StudentSignup html updated

--- Student/Student-Signup.php
+++ Student/Student-Signup.php
@@ -1,7 +1,98 @@
 <?php
-    include "connect-server.php" ;
-?>
+// Include connectserver file
+require_once "../connectserver.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
 
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +101,7 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
 
     <!-- Icon -->
-    <link rel="icon" href="../Images/iiit-logo.png" sizes="35x35" type="image/png">
+    <link rel="icon" href="Images/iiit-logo.png" sizes="35x35" type="image/png">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
@@ -20,8 +111,8 @@
     <link rel="stylesheet" href="CSS/navbar.css">
     <link rel="stylesheet" href="CSS/LoginSignup.css">
 
-    <title>IIIT Book-Shop</title>
 
+    <title>IIIT Book-Shop</title>
 </head>
 <body>
 
@@ -42,48 +133,43 @@
             </div>
             <div id="navbar6" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="index.php">Home</a></li>
-                <li><a href="Books.php">Books</a></li>
-                <li><a href="Admin-Login.php">Admin <i class="fa fa-user" style="color: rgb(160, 159, 158)" aria-hidden="true"></i> </a> </li>
+                <li><a href="BHome.php">Home</a></li>
+                <li><a href="../Admin/Admin-Login.php">Admin <i class="fa fa-user" style="color: rgb(160, 159, 158)" aria-hidden="true"></i> </a> </li>
                 <li><a class="logout" href="Student-Login.php">Student-Login <i class="fa fa-sign-in" style="color: blue" aria-hidden="true"></i> </a> </li>
             </ul>
         </div>
       </div>
     </nav>
-    
 
-    <!-- Login-Content-->
-    <div id="lc" class="login-content">
+    <!-- Login Content -->
+    <div class="login-content">
         <h2>Student Signup</h2>
-        <div class="lform" action="" method="post">
+        <div class="lform">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                    <div class="icon"> <i class="fa fa-user" aria-hidden="true"></i> </div>
+                    <input class="form-control" type="text" name="username" placeholder="Username" value="<?php echo $username; ?>">
+                    <span class="help-block"><?php echo $username_err; ?></span>
+                </div>    
+                <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                    <div class="icon"> <i class="fa fa-lock" aria-hidden="true"></i> </div>
+                    <input class="form-control" type="password" name="password" placeholder="Password" >
+                    <span class="help-block"><?php echo $password_err; ?></span>
+                </div>
 
-            <div class="form-field">
-                <div class="icon"> <i class="fa fa-user" aria-hidden="true"></i> </div>
-                <input class="form-input" type="text" name="name" placeholder="Name" required="">
-            </div>
-            <div class="form-field">
-                <div class="icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
-                <input class="form-input" type="text" name="email" placeholder="Email" required="">
-            </div>
-            <div class="form-field">
-                <div class="icon"> <i class="fa fa-id-card" aria-hidden="true"></i> </div>
-                <input class="form-input" type="text" name="id" placeholder="College ID" required="">
-            </div>
-            <div class="form-field">
-                <div class="icon"> <i class="fa fa-lock" aria-hidden="true"></i> </div>
-                <input class="form-input" type="password" name="password" placeholder="Password" required="">
-            </div>
-            <div class="form-field">
-                <div class="icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
-                <input class="form-input" pattern="[0-9]{10}" type="text" name="phonenum" placeholder="Phone Number" required="">
-            </div>
-            <input class="submit-button" type="submit" name="submit" value="SIGN UP" style="font-family: 'Open Sans';"> 
-
-            <p class="signupnow">Already have an account ? <a href="Student-Login.php" style="color:white" >Sign In now</a>
-                <span class="arrow"><i class="fa fa-arrow-right" style="font-size: 13px" aria-hidden="true"></i></span>
-            </p>
+                <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                    <div class="icon"> <i class="fa fa-lock" aria-hidden="true"></i> </div>
+                    <input class="form-control" type="password" name="confirm_password" placeholder="Confirm Password" value="<?php echo $confirm_password; ?>">
+                    <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                </div>
+                
+                <input class="btn btn-primary submit-button" type="submit" value="Login">
+                <p class="signupnow">Already have an account? <a href="Student-Login.php" style="color:white" >Login here</a>
+                    <span class="arrow"><i class="fa fa-arrow-right" style="font-size: 13px" aria-hidden="true"></i></span>
+                </p>
+            </form>
         </div>
-    </div>
+    </div> 
 
 
     <!-- Footer -->
@@ -94,20 +180,11 @@
       </p>
     </footer>
 
-    <?php
-        if(isset($_POST['submit']))
-        {   
-            mysqli_query($db,"INSERT INTO `STUDENT` VALUES('$_POST[name]', '$_POST[email]', '$_POST[id]', '$_POST[password]', '$_POST[phonenum]');");
-        }
-    ?>
-    
-
     <!-- Bootsrtap JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <!-- JavaScript Linked-->
-    <script src="../Javascript/navbar.js"></script>
-
+    <script src="../Javascript/navbar.js"></script>  
 </body>
 </html>
