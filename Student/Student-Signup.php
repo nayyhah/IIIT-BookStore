fa319ffd3b3a0e4b05e3714254e1980161b02990
Neha Jha <70996315+njha07@users.noreply.github.com>
2021-03-09 15:44:19 +0530

    Update Student-Signup.php

--- Student/Student-Signup.php
+++ Student/Student-Signup.php
@@ -3,8 +3,8 @@
 require_once "../connectserver.php";
  
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$username = $email = $phonenum = $password = $confirm_password = "";
+$username_err = $email_err = $phonenum_err = $password_err = $confirm_password_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -12,7 +12,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate username
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
-    } else{
+    } 
+    else{
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = ?";
         
@@ -41,6 +42,72 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
+
+    // Validate Email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter an Email Id.";
+    } 
+    else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = trim($_POST["email"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $email_err = "This Email Id already exists.";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Validate Phone Number
+    if(empty(trim($_POST["phonenum"]))){
+        $phonenum_err = "Please enter a Phone Number.";
+    }
+    else
+    {
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE phonenum = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_phonenum);
+            
+            // Set parameters
+            $param_phonenum = trim($_POST["phonenum"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+
+                $phonenum = trim($_POST["phonenum"]);
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
     
     // Validate password
     if(empty(trim($_POST["password"]))){
@@ -62,23 +129,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($email_err) && empty($phonenum_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username,email,phonenum, password) VALUES (?,?,?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "ssis", $param_username, $param_email, $param_phonenum, $param_password);
             
             // Set parameters
             $param_username = $username;
+            $param_email = $email;
+            $param_phonenum = $phonenum;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: login.php");
+                // Redirect to After Signup Login page
+                header("location: AfterSignup.php");
             } else{
                 echo "Something went wrong. Please try again later.";
             }
@@ -87,7 +156,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
     // Close connection
     mysqli_close($link);
 }
@@ -99,18 +167,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+    <meta name="author" content="Neha Jha">
+    <meta name="description" content="RDBMS Project">
 
     <!-- Icon -->
     <link rel="icon" href="Images/iiit-logo.png" sizes="35x35" type="image/png">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
 
     <!-- CSS Linked -->
     <link rel="stylesheet" href="CSS/navbar.css">
     <link rel="stylesheet" href="CSS/LoginSignup.css">
-
 
     <title>IIIT Book-Shop</title>
 </head>
@@ -133,9 +201,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div id="navbar6" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="BHome.php">Home</a></li>
-                <li><a href="../Admin/Admin-Login.php">Admin <i class="fa fa-user" style="color: rgb(160, 159, 158)" aria-hidden="true"></i> </a> </li>
-                <li><a class="logout" href="Student-Login.php">Student-Login <i class="fa fa-sign-in" style="color: blue" aria-hidden="true"></i> </a> </li>
+                <li><a href="../index.php"><i class="fa fa-home fa-fw" style="font-size: 20px;" aria-hidden="true"></i>Home</a></li>
+                <li><a href="../Admin/Admin-Login.php">Admin <i class="fa fa-user" aria-hidden="true"></i> </a> </li>
+                <li><a href="Student-Login.php">Student-Login <i class="fa fa-sign-in" style="color: blue" aria-hidden="true"></i> </a> </li>
             </ul>
         </div>
       </div>
@@ -151,6 +219,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     <input class="form-control" type="text" name="username" placeholder="Username" value="<?php echo $username; ?>">
                     <span class="help-block"><?php echo $username_err; ?></span>
                 </div>    
+                
+                <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                    <div class="icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
+                    <input class="form-control" type="text" name="email" placeholder="Email" value="<?php echo $email; ?>">
+                    <span class="help-block"><?php echo $email_err; ?></span>
+                </div>
+
+                <div class="form-group <?php echo (!empty($phonenum_err)) ? 'has-error' : ''; ?>">
+                    <div class="icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
+                    <input class="form-control" type="text" name="phonenum" placeholder="Phone Number" >
+                    <span class="help-block"><?php echo $phonenum_err; ?></span>
+                </div>
+
+
                 <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                     <div class="icon"> <i class="fa fa-lock" aria-hidden="true"></i> </div>
                     <input class="form-control" type="password" name="password" placeholder="Password" >
@@ -163,14 +245,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     <span class="help-block"><?php echo $confirm_password_err; ?></span>
                 </div>
                 
-                <input class="btn btn-primary submit-button" type="submit" value="Login">
+                <input class="btn btn-primary submit-button" type="submit" value="Sign Up">
                 <p class="signupnow">Already have an account? <a href="Student-Login.php" style="color:white" >Login here</a>
                     <span class="arrow"><i class="fa fa-arrow-right" style="font-size: 13px" aria-hidden="true"></i></span>
                 </p>
             </form>
         </div>
     </div> 
-
 
     <!-- Footer -->
     <footer id="footer" class="footer">
