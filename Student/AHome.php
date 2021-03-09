bd10b9ade5304782e2b77b0e99837eb693782a20
Neha Jha <70996315+njha07@users.noreply.github.com>
2021-03-09 15:41:27 +0530

    Update AHome.php

--- Student/AHome.php
+++ Student/AHome.php
@@ -4,6 +4,8 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <meta name="author" content="Neha Jha">
+    <meta name="description" content="RDBMS Project">
 
     <!-- Icon -->
     <link rel="icon" href="Images/iiit-logo.png" sizes="35x35" type="image/png">
@@ -34,10 +36,11 @@
             </div>
             <div id="navbar6" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="AHome.php">Home</a></li>
-                <li><a href="Books.php">Books</a></li>
-                <li><a href="Profile.php">Profile <i class="fa fa-user" style="color: rgb(160, 159, 158)" aria-hidden="true"></i> </a> </li>
+                <li class="active"><a href="AHome.php"><i class="fa fa-home fa-fw" style="font-size: 20px;" aria-hidden="true"></i>Home</a></li>
+                <li><a href="Books.php">Books <i class="fa fa-bookmark-o" aria-hidden="true"></i></a></li>
+                <li><a href="BookCart.php">BookCart <i class="fa fa-shopping-cart"  aria-hidden="true"></i> </a></li>
                 <li><a class="logout" href="Student-Logout.php">Logout <i class="fa fa-sign-out" style="color: blue" aria-hidden="true"></i> </a> </li>
+                <a href="Reset-Password.php" class="btn btn-danger" style="color: white; font-weight: normal; font-size: 14px; margin-top:24px; background-color: #6c6d6e;border: none;">Reset-Password </a>
             </ul>
         </div>
       </div>
@@ -64,5 +67,6 @@
 
     <!-- JavaScript Linked-->
     <script src="Javascript/navbar.js"></script>
+    
   </body>
 </html>
