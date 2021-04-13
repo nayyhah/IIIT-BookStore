cf10cee1540d615cfbd67660d3c1284e978236f8
Neha Jha <70996315+njha07@users.noreply.github.com>
2021-04-13 16:16:08 +0530

    Thumbnail added

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <meta name="author" content="Neha Jha">
     <meta name="description" content="RDBMS Project">
+    <meta name="title" property="og:title" content="IIIT BookStore" />
+    <meta property="og:type" content="Website" />
+    <meta 
+	      name="image" 
+	      property="og:image" 
+	      content="https://live.staticflickr.com/65535/51113060029_6b6dc4aaf9_z.jpg" 
+    />
     
     <!-- Icon -->
     <link rel="icon" href="Student/Images/iiit-logo.png" sizes="35x35" type="image/png">
